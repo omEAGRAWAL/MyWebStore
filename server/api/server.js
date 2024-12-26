7c706b963a38ddfdf164f37345a399d72d41e106
sur-source <suryapt.sing@gmail.com>
2024-12-26 16:41:45 +0530

    port3000

--- server/api/server.js
+++ server/api/server.js
@@ -3,6 +3,15 @@ const app = express();
 
 app.get('/', (req, res) => {
     res.send('Hello, Vercel!');
+
 });
 
+
+
+
 module.exports = app; // Export the app
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+}
+);
